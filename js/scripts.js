2c41de1b1da35bc735350d27096774e0f11bff13
willwv <william.walder@gmail.com>
2021-03-20 18:04:23 -0300

    implementação da area de textos em destaque

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.backgroundColor = "#242C40";
+    } else {
+    document.getElementById("header").style.backgroundColor = "transparent";
+    }
+}
