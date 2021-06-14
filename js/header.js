35b6942f28650c3fa203a8c9d9ebea5f6a64ee61
William Walder <william.walder@gmail.com>
2021-06-14 20:33:13 -0300

    ajustes de script e favicon

--- js/header.js
+++ js/header.js
@@ -1,22 +1,20 @@
-window.onload = function() {
-    window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.backgroundColor = "#242C40";
-        } else {
-        document.getElementById("header").style.backgroundColor = "transparent";
-        }
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.backgroundColor = "#242C40";
+    } else {
+    document.getElementById("header").style.backgroundColor = "transparent";
     }
-    
+}
+
+document.getElementsByClassName("all-options")[0].style.display = 'none';
+document.getElementsByClassName("options-mobile")[0].onclick = function(){
+    document.getElementsByClassName("all-options")[0].style.display = 'flex';
+};
+document.getElementsByClassName("options-close")[0].onclick = function(){
     document.getElementsByClassName("all-options")[0].style.display = 'none';
-    document.getElementsByClassName("options-mobile")[0].onclick = function(){
-        document.getElementsByClassName("all-options")[0].style.display = 'flex';
-    };
-    document.getElementsByClassName("options-close")[0].onclick = function(){
-        document.getElementsByClassName("all-options")[0].style.display = 'none';
-    };
-    document.getElementsByClassName(".options-all")[0].onclick = function(){
-        document.getElementsByClassName("all-options")[0].style.display = 'none';
-    };
+};
+document.getElementsByClassName(".options-all")[0].onclick = function(){
+    document.getElementsByClassName("all-options")[0].style.display = 'none';
 };
