7b7da390031651caef28a5fabf09111dfb5af51e
William Walder <william.walder@gmail.com>
2021-06-18 00:58:07 -0300

    criação de blog

--- js/header.js
+++ js/header.js
@@ -1,10 +1,13 @@
 window.onscroll = function() {scrollFunction()};
+if(window.location.pathname != "/"){
+    document.getElementById("header").style.backgroundColor = "#242C40";
+}
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.backgroundColor = "#242C40";
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 || window.location.pathname != "/") {
+        document.getElementById("header").style.backgroundColor = "#242C40";
     } else {
-    document.getElementById("header").style.backgroundColor = "transparent";
+        document.getElementById("header").style.backgroundColor = "transparent";
     }
 }
 
@@ -18,3 +21,28 @@ document.getElementsByClassName("options-close")[0].onclick = function(){
 document.getElementsByClassName("options-all")[0].onclick = function(){
     document.getElementsByClassName("all-options")[0].style.display = 'none';
 };
+
+document.getElementById("header-home").onclick = () => headerGoTo("home");
+document.getElementById("header-escritorio").onclick = () => headerGoTo("escritorio");
+document.getElementById("header-atendimento").onclick = () => headerGoTo("atendimento");
+document.getElementById("header-atuacao").onclick = () => headerGoTo("atuacao");
+document.getElementById("header-sobre").onclick = () => headerGoTo("sobre");
+document.getElementById("header-blog").onclick = () => headerGoTo("blog");
+document.getElementById("header-mobile-home").onclick = () => headerGoTo("home");
+document.getElementById("header-mobile-escritorio").onclick = () => headerGoTo("escritorio");
+document.getElementById("header-mobile-atendimento").onclick = () => headerGoTo("atendimento");
+document.getElementById("header-mobile-atuacao").onclick = () => headerGoTo("atuacao");
+document.getElementById("header-mobile-sobre").onclick = () => headerGoTo("sobre");
+document.getElementById("header-mobile-blog").onclick = () => headerGoTo("blog");
+
+function headerGoTo(divId){
+    if(window.location.pathname == "/"){
+        let element = document.getElementById(divId);
+
+        const y = element.getBoundingClientRect().top + window.pageYOffset -97;
+        window.scrollTo({top: y, behavior: 'smooth'});
+        // element.scrollIntoView() -= 97;
+    }else{
+        window.location.href = window.location.origin + "/#" + divId;
+    }
+}
---
+++ js/page-blog.js
@@ -0,0 +1,52 @@
+window.onload = function(){
+    loadDocument("blog-page-include-header", "/components/", "header", true);
+    loadDocument("blog-page-include-footer", "/components/", "footer", false);
+    fitText();
+    function loadDocument (id, documentFolder, documentName, importJs, e) {
+        (e || window.event).preventDefault();
+    
+        fetch(window.location.origin + documentFolder + documentName + ".html")
+        .then((response) => response.text())
+        .then((html) => {
+            document.getElementById(id).innerHTML = html;
+            if(importJs){
+                var script = document.createElement('script');
+                script.src = window.location.origin + "/js/" + documentName + ".js";
+                document.getElementsByTagName('body')[0].appendChild(script);
+            }
+        })
+        .catch((error) => {
+            console.warn(error);
+        });
+    }
+    function fitText(){
+        let textName = getParameterByName('text');
+        readTextFile(textName);
+    }
+    function readTextFile(documentName)
+    {
+        let rawFile = new XMLHttpRequest();
+        let filePath =  window.location.origin + "/texts/" + documentName + ".txt";
+        console.log(filePath);
+        rawFile.open("GET", filePath, false);
+        rawFile.onreadystatechange = function ()
+        {
+            if(rawFile.readyState === 4)
+            {
+                if(rawFile.status === 200 || rawFile.status == 0)
+                {
+                    document.getElementsByClassName("blog-texto-fit")[0].innerText = rawFile.responseText;
+                }
+            }
+        }
+        rawFile.send(null);
+    }
+    function getParameterByName(name, url = window.location.href) {
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+}
